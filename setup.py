db994b25b8c8b890c871f232ee872f7dca98eb30
Sristy Sharma <45727820+beertocode@users.noreply.github.com>
2021-10-20 22:52:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'MA691_COBRA_12',         # How you named your package folder (MyLib)
   packages = ['MA691_COBRA_12'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '0.6',      # Start with a small number and increase it with every change you make
   license='AFL',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Regressions on Boston dataset',   # Give a short description about your library
   author = 'Sristy',                   # Type in your name
   author_email = 'sristy.sharma98@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/beertocode/MA691_COBRA_12',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/beertocode/MA691_COBRA_12/archive/refs/tags/0.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/beertocode/MA691_COBRA_12/archive/refs/tags/0.6.tar.gz',    # I explain this later on
   keywords = ['Cobra', 'Ridge', 'Lasso','KNN','Kernel Cobra','MLR'],   # Keywords that define your package best
   install_requires=[           
           'numpy',
